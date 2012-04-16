4344e70d702f70453595f95438ba466227e2c1d1
Thomas Sievert <thomas.t.m.sievert@gmail.com>
2012-04-16 16:20:23 +0200

    Added Level class

---
+++ Code/Logic/Level.cpp
@@ -0,0 +1,173 @@
+#include "Level.hpp"
+#include "FreeImage.h"
+#include <cassert>
+#include <algorithm>
+
+namespace Model
+{
+	Coord::Coord()
+		: X(0), Y(0){}
+
+	Coord::Coord(int x, int y)
+		: X(x), Y(y) {}
+
+	bool Coord::operator==(const Coord& rhs) const
+	{
+		return (X == rhs.X && Y == rhs.Y);
+	}
+
+	Cell::Cell()
+		: Coordinate(0, 0), Type(0) {}
+
+	Cell::Cell(int x, int y, CellType type)
+		: Coordinate(x, y), Type(type) {}
+
+	Cell::Cell(Coord coordinate, CellType type)
+		: Coordinate(coordinate), Type(type) {}
+
+	Level::Level(const std::string& path)
+	{
+		FIBITMAP *imgFile = FreeImage_Load(FIF_PNG, path.c_str(), PNG_DEFAULT);
+		mHeight = FreeImage_GetHeight(imgFile);
+		mWidth = FreeImage_GetWidth(imgFile);
+
+		for (int i = mHeight - 1; i >= 0; --i)
+		{
+			for (unsigned int j = 0; j < mWidth; ++j)
+			{
+				RGBQUAD color;
+			
+				BOOL a = FreeImage_GetPixelColor(imgFile, j, i, &color);
+				assert(a != 0);
+
+				if (color.rgbRed == 255)
+				{
+					if (color.rgbGreen == 255)
+					{
+						if (color.rgbBlue == 255)
+						{
+							Cell c = Cell(Coord(j, i), Cell::C_CELLTYPE_EMPTY); 
+							mCells.push_back(c);
+						}
+						else
+						{
+							Cell c = Cell(Coord(j, i), Cell::C_CELLTYPE_GHOSTSPAWN);
+							mCells.push_back(c);
+							mGhostSpawnPositions.push_back(c.Coordinate);
+						}
+					}
+					else
+					{
+						Cell c = Cell(Coord(j, i), Cell::C_CELLTYPE_POWERPELLET);
+						mCells.push_back(c);
+						mPowerPelletPositions.push_back(c.Coordinate);
+					}
+				}
+				else if (color.rgbGreen == 255)
+				{
+					if (color.rgbBlue == 255)
+					{
+						Cell c = Cell(Coord(j, i), Cell::C_CELLTYPE_WALL);
+						mCells.push_back(c);
+						mLightPositions.push_back(c.Coordinate);
+					}
+					else
+					{
+						Cell c = Cell(Coord(j, i), Cell::C_CELLTYPE_PELLET);
+						mCells.push_back(c);
+						mPelletPositions.push_back(c.Coordinate);
+					}
+				}
+				else if (color.rgbBlue == 255)
+				{
+					Cell c = Cell(Coord(j, i), Cell::C_CELLTYPE_PACMANSPAWN);
+					mCells.push_back(c);
+					mPacmanSpawnPosition = c.Coordinate;
+				}
+				else
+				{
+					Cell c = Cell(Coord(j, i), Cell::C_CELLTYPE_WALL);
+					mCells.push_back(c);
+					mWallPositions.push_back(c.Coordinate);
+				}
+			}
+		}
+
+	}
+
+	void Level::AddFood()
+	{
+		SetCellType(GetPacmanSpawnPosition().X, GetPacmanSpawnPosition().Y, Cell::C_CELLTYPE_FOOD);
+	}
+
+	void Level::RemoveFood()
+	{
+		SetCellType(GetPacmanSpawnPosition().X, GetPacmanSpawnPosition().Y, Cell::C_CELLTYPE_EMPTY);
+	}
+
+	void Level::SetEaten(int x, int y)
+	{
+		Cell c = GetCell(x, y);
+		if (c.Type == Cell::C_CELLTYPE_PELLET)
+		{
+			std::vector<Coord>::iterator it = std::find(mPelletPositions.begin(), mPelletPositions.end(), c.Coordinate);
+			mPelletPositions.erase(it);
+		}
+		else if (c.Type == Cell::C_CELLTYPE_POWERPELLET)
+		{
+			std::vector<Coord>::iterator it = std::find(mPowerPelletPositions.begin(), mPowerPelletPositions.end(), c.Coordinate);
+			mPelletPositions.erase(it);
+		}
+		SetCellType(x, y, Cell::C_CELLTYPE_EMPTY);
+	}
+
+	Cell Level::GetCell(int x, int y) const
+	{
+		return mCells[GetIndex(x, y)];
+	}
+
+	bool Level::FoodExists() const
+	{
+		return (GetCell(GetPacmanSpawnPosition().X, GetPacmanSpawnPosition().Y).Type == Cell::C_CELLTYPE_FOOD);
+	}
+
+	Coord Level::GetPacmanSpawnPosition() const
+	{
+		return mPacmanSpawnPosition;
+	}
+
+	const std::vector<Coord>& Level::GetWallPositions() const
+	{
+		return mWallPositions;
+	}
+
+	const std::vector<Coord>& Level::GetLightPositions() const
+	{
+		return mLightPositions;
+	}
+
+	const std::vector<Coord>& Level::GetPelletPositions() const
+	{
+		return mPelletPositions;
+	}
+
+	const std::vector<Coord>& Level::GetPowerPelletPositions() const
+	{
+		return mPowerPelletPositions;
+	}
+
+	const std::vector<Coord>& Level::GetGhostSpawnPositions() const
+	{
+		return mGhostSpawnPositions;
+	}
+
+	int Level::GetIndex(int x, int y) const
+	{
+		return x + y * mHeight;
+	}
+
+	void Level::SetCellType(int x, int y, Cell::CellType type)
+	{
+		mCells[GetIndex(x, y)].Type = type;
+	}
+}
