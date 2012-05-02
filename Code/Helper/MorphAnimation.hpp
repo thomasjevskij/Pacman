e621a0bb59afd4a2508b4e4192d784d3248c24c3
Thomas Sievert <thomas.t.m.sievert@gmail.com>
2012-05-02 13:42:34 +0200

    Added animation files

---
+++ Code/Helper/MorphAnimation.hpp
@@ -0,0 +1,22 @@
+#ifndef MORPH_ANIMATION_HPP
+#define MORPH_ANIMATION_HPP
+
+#include <vector>
+#include "Global.hpp"
+#include "Camera.hpp"
+
+namespace Helper
+{
+	class MorphAnimation
+	{
+	public:
+
+
+		void Update(float dt);
+		void Draw(const Camera& camera, D3DXVECTOR3 position);
+	private:
+
+	};
+}
+
+#endif
